10af7d7d2f31da052e1c4fc4c6a3fef09fc86f53
furkan izmir <f.izmir03@gmail.com>
2025-02-18 13:58:50 +0300

    yeniden

--- CSEgitim301_Presentation/Program.cs
+++ CSEgitim301_Presentation/Program.cs
@@ -16,7 +16,7 @@ namespace CSEgitim301_Presentation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmProduct());
         }
     }
 }
